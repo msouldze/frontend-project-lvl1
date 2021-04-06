81d775e4e009c3ed3f5b26c5576557fbf669fcd8
Aiji <msouldze1@gmail.com>
2021-04-06 16:55:38 +0900

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import readlineSync from 'readline-sync';
+
+// Greetings
+let name;
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  name = readlineSync.question('May I have your name? ');
+  return `Hello, ${name}!`;
+};
+
+// Get a random number
+const getRandomNumber = () => Math.ceil(Math.random() * 100);
+
+// Question for player
+const question = (argument) => `Question: ${argument}`;
+
+export { greeting, getRandomNumber, question, name };
