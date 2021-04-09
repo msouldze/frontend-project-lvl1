6adcb2160a528a247705ffb5a770e135b5a03987
Aiji <msouldze1@gmail.com>
2021-04-09 16:32:29 +0900

    Add new game brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import {
+  greeting, getRandomNumber, question, loss, win, isPrime,
+} from '../src/index.js';
+
+// Welcoming and a request for player's name
+console.log(greeting());
+
+// Introducing game's rule
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+let answerCount = 0;
+
+for (let i = 0; i < 3; i += 1) {
+  const randomNumber = getRandomNumber();
+  console.log(question(randomNumber));
+
+  // Await an answer
+  const answer = readlineSync.question('Your answer: ');
+  const result = isPrime(randomNumber);
+  if (
+    (result === true && answer === 'yes')
+    || (result === false && answer === 'no')
+  ) {
+    console.log('Correct!');
+    answerCount += 1;
+  } else if (result === true && answer === 'no') {
+    console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'.`);
+    console.log(loss());
+  } else {
+    console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.`);
+    console.log(loss());
+  }
+}
+if (answerCount === 3) {
+  console.log(win());
+}
