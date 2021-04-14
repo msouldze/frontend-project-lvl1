9cf15441dd9a369193d846b26c24d49be9970515
Aiji <msouldze1@gmail.com>
2021-04-14 15:58:24 +0900

    modify prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,7 +2,7 @@ export default (number) => {
   if (number === 1) {
     return false;
   }
-  for (let i = 2; i < number / 2; i += 1) {
+  for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       return false;
     }
