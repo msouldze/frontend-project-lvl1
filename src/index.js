a0fd4a9121d35f2216ecc0a8bea4c89f5ed4c823
Aiji <msouldze1@gmail.com>
2021-04-23 22:21:23 +0900

    refactor: add roundCount and fix code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import readlineSync from 'readline-sync';
-import { numberOfGamesCount } from './utilities.js';
 
-export default (gameRule, game) => {
+const roundsCount = 3;
+
+export default (game) => {
+  const { gameRule, getRound } = game();
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(gameRule);
 
-  for (let i = 0; i < numberOfGamesCount; i += 1) {
-    const [result, question] = game();
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [question, answer] = getRound();
     console.log(`Question: ${question}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (result !== answer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.`);
+    const result = readlineSync.question('Your answer: ');
+    if (answer !== result) {
+      console.log(`'${result}' is wrong answer ;(. Correct answer was '${answer}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
